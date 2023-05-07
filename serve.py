4ca28ecff900bf54b058509c16967e9551a21e30
Ming <mingyuan.ren@gmail.com>
2023-05-06 20:47:04 -0700

    Update serve.py

--- serve.py
+++ serve.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 path = os.path.dirname(os.path.abspath(__file__))
 strees_cpu_file_path = os.path.join(path,"stress_cpu.py")
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
 @app.route('/',methods = ['POST'])
 def run_stress_cpu():
    p = subprocess.Popen(["python3",strees_cpu_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -25,5 +21,5 @@ def get_seed():
    return str(myIP)
 
 if __name__ == '__main__':
-   run_stress_cpu()
-   #app.run(host='0.0.0.0',debug = True)
+   #run_stress_cpu()
+   app.run(host='0.0.0.0',debug = True)
